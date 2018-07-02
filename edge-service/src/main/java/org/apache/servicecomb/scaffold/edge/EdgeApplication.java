2dab49cc170cf62c086e68d0635609edeb938e2b
zhengyangyong <yangyong.zheng@huawei.com>
2018-07-02 11:29:55 +0800

    add edge service implement
    Signed-off-by: zhengyangyong <yangyong.zheng@huawei.com>

---
+++ edge-service/src/main/java/org/apache/servicecomb/scaffold/edge/EdgeApplication.java
@@ -0,0 +1,11 @@
+package org.apache.servicecomb.scaffold.edge;
+
+import org.apache.servicecomb.foundation.common.utils.BeanUtils;
+import org.apache.servicecomb.foundation.common.utils.Log4jUtils;
+
+public class EdgeApplication {
+  public static void main(String[] args) throws Exception {
+    Log4jUtils.init();
+    BeanUtils.init();
+  }
+}
---
+++ edge-service/src/main/java/org/apache/servicecomb/scaffold/edge/EdgeDispatcher.java
@@ -0,0 +1,53 @@
+package org.apache.servicecomb.scaffold.edge;
+
+import java.util.Map;
+
+import org.apache.servicecomb.edge.core.AbstractEdgeDispatcher;
+import org.apache.servicecomb.edge.core.EdgeInvocation;
+import org.apache.servicecomb.serviceregistry.definition.DefinitionConst;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.config.DynamicPropertyFactory;
+
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.CookieHandler;
+
+public class EdgeDispatcher extends AbstractEdgeDispatcher {
+  private static final Logger LOGGER = LoggerFactory.getLogger(EdgeDispatcher.class);
+
+  //此Dispatcher的优先级，Order级越小，路由策略优先级越高
+  public int getOrder() {
+    return 10000;
+  }
+
+  //初始化Dispatcher的路由策略
+  public void init(Router router) {
+    ///捕获 {ServiceComb微服务Name}/{服务路径&参数} 的URL
+    String regex = "/([^\\\\/]+)/(.*)";
+    router.routeWithRegex(regex).handler(CookieHandler.create());
+    router.routeWithRegex(regex).handler(createBodyHandler());
+    router.routeWithRegex(regex).failureHandler(this::onFailure).handler(this::onRequest);
+  }
+
+  //处理请求，请注意
+  private void onRequest(RoutingContext context) {
+    Map<String, String> pathParams = context.pathParams();
+    //从匹配的param0拿到{ServiceComb微服务Name}
+    final String service = pathParams.get("param0");
+    //从匹配的param1拿到{服务路径&参数}
+    String path = "/" + pathParams.get("param1");
+
+    //还记得我们之前说的做出一点点改进吗？引入一个自定义配置edge.routing-short-path.{简称}，映射微服务名；如果简称没有配置，那么就认为直接是微服务的名
+    final String serviceName = DynamicPropertyFactory.getInstance()
+        .getStringProperty("edge.routing-short-path." + service, service).get();
+
+    //创建一个Edge转发
+    EdgeInvocation edgeInvocation = new EdgeInvocation();
+    //允许接受任意版本的微服务实例作为Provider，未来我们会使用此（设置版本）能力实现灰度发布
+    edgeInvocation.setVersionRule(DefinitionConst.VERSION_RULE_ALL);
+    edgeInvocation.init(serviceName, context, path, httpServerFilters);
+    edgeInvocation.edgeInvoke();
+  }
+}

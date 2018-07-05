6a800b34366882d996cf2deb3b2c0b4bd2120e7e
zhengyangyong <yangyong.zheng@huawei.com>
2018-07-05 09:41:39 +0800

    add authentication handler implement
    Signed-off-by: zhengyangyong <yangyong.zheng@huawei.com>

--- edge-service/src/main/java/org/apache/servicecomb/scaffold/edge/EdgeDispatcher.java
+++ edge-service/src/main/java/org/apache/servicecomb/scaffold/edge/EdgeDispatcher.java
@@ -1,10 +1,19 @@
 package org.apache.servicecomb.scaffold.edge;
 
+import static org.springframework.http.HttpHeaders.CONTENT_LENGTH;
+
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.ws.rs.core.Response.Status;
 
 import org.apache.servicecomb.edge.core.AbstractEdgeDispatcher;
 import org.apache.servicecomb.edge.core.EdgeInvocation;
+import org.apache.servicecomb.foundation.common.utils.SPIServiceUtils;
 import org.apache.servicecomb.serviceregistry.definition.DefinitionConst;
+import org.apache.servicecomb.swagger.invocation.exception.InvocationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +25,12 @@ import io.vertx.ext.web.handler.CookieHandler;
 
 public class EdgeDispatcher extends AbstractEdgeDispatcher {
   private static final Logger LOGGER = LoggerFactory.getLogger(EdgeDispatcher.class);
+
+  private final List<EdgeFilter> filterChain;
+
+  public EdgeDispatcher() {
+    filterChain = SPIServiceUtils.getSortedService(EdgeFilter.class);
+  }
 
   //此Dispatcher的优先级，Order级越小，路由策略优先级越高
   public int getOrder() {
@@ -37,7 +52,7 @@ public class EdgeDispatcher extends AbstractEdgeDispatcher {
     //从匹配的param0拿到{ServiceComb微服务Name}
     final String service = pathParams.get("param0");
     //从匹配的param1拿到{服务路径&参数}
-    String path = "/" + pathParams.get("param1");
+    String operationPath = "/" + pathParams.get("param1");
 
     //还记得我们之前说的做出一点点改进吗？引入一个自定义配置edge.routing-short-path.{简称}，映射微服务名；如果简称没有配置，那么就认为直接是微服务的名
     final String serviceName = DynamicPropertyFactory.getInstance()
@@ -47,7 +62,43 @@ public class EdgeDispatcher extends AbstractEdgeDispatcher {
     EdgeInvocation edgeInvocation = new EdgeInvocation();
     //允许接受任意版本的微服务实例作为Provider，未来我们会使用此（设置版本）能力实现灰度发布
     edgeInvocation.setVersionRule(DefinitionConst.VERSION_RULE_ALL);
-    edgeInvocation.init(serviceName, context, path, httpServerFilters);
-    edgeInvocation.edgeInvoke();
+    edgeInvocation.init(serviceName, context, operationPath, httpServerFilters);
+
+    //处理Filter链并转发请求
+    loopExecuteEdgeFilterInChain(0, serviceName, operationPath, context, edgeInvocation);
+  }
+
+  private void loopExecuteEdgeFilterInChain(int index, String serviceName, String operationPath, RoutingContext context,
+      EdgeInvocation edgeInvocation) {
+    if (index < filterChain.size()) {
+      EdgeFilter filter = filterChain.get(index);
+      AtomicReference<InvocationException> exception = new AtomicReference<>();
+      CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+        try {
+          filter.processing(serviceName, operationPath, context);
+        } catch (InvocationException e) {
+          exception.set(e);
+        }
+      });
+
+      future.whenComplete((result, throwable) -> {
+        if (exception.get() != null) {
+          sendFailed(context, exception.get());
+        } else if (throwable != null) {
+          sendFailed(context, new InvocationException(Status.INTERNAL_SERVER_ERROR, throwable.getMessage()));
+        } else {
+          loopExecuteEdgeFilterInChain(index + 1, serviceName, operationPath, context, edgeInvocation);
+        }
+      });
+    } else {
+      edgeInvocation.edgeInvoke();
+    }
+  }
+
+  private void sendFailed(RoutingContext context, InvocationException exception) {
+    context.response().setStatusCode(exception.getStatusCode());
+    context.response().headers().add(CONTENT_LENGTH, String.valueOf(exception.getMessage().length()));
+    context.response().write(exception.getMessage());
+    context.response().end();
   }
 }
---
+++ edge-service/src/main/java/org/apache/servicecomb/scaffold/edge/filter/AuthenticationFilter.java
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.servicecomb.scaffold.edge.filter;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Response.Status;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.servicecomb.provider.springmvc.reference.RestTemplateBuilder;
+import org.apache.servicecomb.scaffold.edge.EdgeFilter;
+import org.apache.servicecomb.swagger.invocation.exception.InvocationException;
+import org.springframework.web.client.RestTemplate;
+
+import io.vertx.ext.web.RoutingContext;
+
+public class AuthenticationFilter implements EdgeFilter {
+
+  private final RestTemplate template = RestTemplateBuilder.create();
+
+  private static final String USER_SERVICE_NAME = "user-service";
+
+  public static final String EDGE_AUTHENTICATION_NAME = "edge-authentication-name";
+
+  private static final Set<String> NOT_REQUIRED_VERIFICATION_USER_SERVICE_METHODS = new HashSet<>(
+      Arrays.asList("/login", "/logon", "/validate"));
+
+  @Override
+  public int getOrder() {
+    return 0;
+  }
+
+  @Override
+  public void processing(String serviceName, String operationPath, RoutingContext context) throws InvocationException {
+    if (isNotRequiredVerificationUserServiceMethod(serviceName, operationPath)) {
+      return;
+    }
+
+    String token = context.request().headers().get(AUTHORIZATION);
+    if (StringUtils.isNotEmpty(token)) {
+      String userName = template
+          .getForObject("cse://" + USER_SERVICE_NAME + "/validate?token={token}", String.class, token);
+      if (StringUtils.isNotEmpty(userName)) {
+        //Add header
+        context.request().headers().add(EDGE_AUTHENTICATION_NAME, userName);
+      } else {
+        throw new InvocationException(Status.UNAUTHORIZED, "authentication failed, invalid token");
+      }
+    } else {
+      throw new InvocationException(Status.UNAUTHORIZED, "authentication failed, missing AUTHORIZATION header");
+    }
+  }
+
+  private boolean isNotRequiredVerificationUserServiceMethod(String serviceName, String operationPath) {
+    if (USER_SERVICE_NAME.equals(serviceName)) {
+      for (String method : NOT_REQUIRED_VERIFICATION_USER_SERVICE_METHODS) {
+        if (operationPath.startsWith(method)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+}
